ea0959cb816374e2c234bda09270306773e0d18e
Michael Lage <gs.urmomma@gmail.com>
2020-08-11 17:48:28 -0600

    WIP
    - site layout
    - introduction page updated
    - linking to static based images, not repo ones

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
-        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
-        baseDir: `examples/gatsby-theme-docs`,
+        githubUrl: `https://github.com/mrWh1te/botmation-docs`,
+        baseDir: ``,
       },
     },
     {
