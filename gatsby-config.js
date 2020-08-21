9e321e06fe0c63e0849cae86ca2bf00b7f4f74e1
Michael Lage <gs.urmomma@gmail.com>
2020-08-21 15:52:08 -0600

    GA Tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: ``,
+        trackingId: `UA-1552765-2`,
       },
     },
     {
